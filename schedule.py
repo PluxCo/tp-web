6044beb2304439f2193fb1883cdf623af354b548
Anton Chuvashov <antonchuvashow@gmail.com>
2023-07-21 14:52:24 +0500

    completed basic schedule creator

--- schedule.py
+++ schedule.py
@@ -2,6 +2,7 @@ import datetime
 import enum
 import math, time
 import numpy as np
+from collections import defaultdict
 
 from sqlalchemy import select, and_, or_
 from faker import Faker
@@ -66,26 +67,55 @@ def fake_db(session: Session, scale=100):
 def random_question(person_id: int, group_id: list or int = None) -> int:
     """Gives random question id within the same group, which hasn't been asked yet otherwise returns -1"""
 
+    with create_session() as db:
+        person = db.scalar(select(users.Person).where(users.Person.id == person_id))
+        if group_id is None:
+            groups = [x.id for x in person.groups]
+        elif group_id is int:
+            groups = [group_id]
+        else:
+            groups = group_id
+        groups: list
+
+        question_ids = [x.id for x in db.scalars(
+            select(questions.Question).join(questions.Question.groups).where(users.PersonGroup.id.in_(groups)))]
+        answered_question_ids = [x.question_id for x in db.scalars(
+            select(questions.QuestionAnswer).where(questions.QuestionAnswer.person_id == person_id))]
+
+        result_list = list(set(question_ids).difference(answered_question_ids))
+        if len(result_list) > 0:
+            return np.random.choice(result_list)
+        else:
+            return -1
+
+
+def repetition_function(person_id: int, now=datetime.datetime.now()):
+    repetition_amount = 6
+
+    def period_function(x):
+        return math.pow(2, x)
+
     db = create_session()
-    person = db.scalar(select(users.Person).where(users.Person.id == person_id))
-    if group_id is None:
-        groups = [x.id for x in person.groups]
-    elif group_id is int:
-        groups = [group_id]
-    else:
-        groups = group_id
-    groups: list
+    answers_map = defaultdict(list)
+    questions_to_ask = []
+    questions_to_ask_now = []
 
-    question_ids = [x.id for x in db.scalars(
-        select(questions.Question).join(questions.Question.groups).where(users.PersonGroup.id.in_(groups)))]
-    answered_question_ids = [x.question_id for x in db.scalars(
-        select(questions.QuestionAnswer).where(questions.QuestionAnswer.person_id == person_id))]
+    answers_timeline = db.scalars(
+        select(questions.QuestionAnswer).where(questions.QuestionAnswer.person_id == person_id).order_by(
+            questions.QuestionAnswer.ask_time.desc())).all()
 
-    result_list = list(set(question_ids).difference(answered_question_ids))
-    if len(result_list) > 0:
-        return np.random.choice(result_list)
-    else:
-        return -1
+    for answer in answers_timeline:
+        answers_map[answer.question_id].append(answer)
+
+    for key in answers_map:
+        length = len(answers_map[key])
+        if length < repetition_amount:
+            delta = datetime.timedelta(period_function(length))
+            questions_to_ask.append((key, delta))
+            if delta + answers_map[key][0].ask_time - datetime.timedelta(seconds=1) <= now <= delta + answers_map[key][0].ask_time + datetime.timedelta(seconds=1):
+                questions_to_ask_now.append(key)
+
+    return questions_to_ask_now, questions_to_ask
 
 
 class WeekDays(enum.Enum):
@@ -105,8 +135,8 @@ class Schedule(Thread):
         self._every = {}
         self._order = None  # 1 if time period is calculated first and 0 in other case
         self._week_days = None
-        self._distribution_function = math.exp
-        self._repetition_amount = 6
+        self._distribution_function = None
+        self._repetition_amount = None
 
     def every(self, seconds: float = 0, minutes: float = 0, hours: float = 0,
               days: float = 0, weeks: float = 0, months: float = 0):
@@ -129,31 +159,53 @@ class Schedule(Thread):
 
         return self
 
+    def using_repetition_function(self, distribution_function=math.exp, repetition_amount=6):
+        self._distribution_function = distribution_function
+        self._repetition_amount = repetition_amount
+
+        return self
+
     def run(self) -> None:
         """The run function of a schedule thread. Note that the order in which you call methods matters.
          on().every() and every().on() play different roles. They in somewhat way mask each-other."""
-
+        db = create_session()
         previous_call = None
         while True:
+            persons = db.scalars(select(users.Person)).all()
+            question_for_person = {}
             now = datetime.datetime.now()
-
-            if self._order == 1:
-                if previous_call is None or ('month' in self._every.keys() and (
-                        (previous_call.month + self._every['month']) % 12 + 1) >= now.month):
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
-                        previous_call = now
-                        if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                            self._callback()
-                            previous_call = now
-                elif 'month' not in self._every.keys():
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
-                        previous_call = now
-                        if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                            self._callback()
-                            previous_call = now
-            if self._order == 0:
-                if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
-                        self._callback()
-                        previous_call = now
+            if self._repetition_amount is not None:
+                for person in persons:
+                    questions_to_ask_now = repetition_function(person.id, now)[0]
+                    if len(questions_to_ask_now) != 0:
+                        question_for_person[person.id] = np.random.choice(questions_to_ask_now)
+                    else:
+                        question_for_person[person.id] = random_question(person.id)
+                previous_call = self._periodic_call(now, previous_call, question_for_person)
+            else:
+                for person in persons:
+                    question_for_person[person.id] = random_question(person.id)
+                previous_call = self._periodic_call(now, previous_call, question_for_person)
             time.sleep(1)
+
+    def _periodic_call(self, now, previous_call, args):
+        if self._order == 1:
+            if previous_call is None or ('month' in self._every.keys() and (
+                    (previous_call.month + self._every['month']) % 12 + 1) >= now.month):
+                if previous_call is None or (now >= previous_call + self._every['delta']):
+                    previous_call = now
+                    if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
+                        self._callback(args)
+                        previous_call = now
+            elif 'month' not in self._every.keys():
+                if previous_call is None or (now >= previous_call + self._every['delta']):
+                    previous_call = now
+                    if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
+                        self._callback(args)
+                        previous_call = now
+        if self._order == 0:
+            if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
+                if previous_call is None or (now >= previous_call + self._every['delta']):
+                    self._callback(args)
+                    previous_call = now
+        return previous_call
