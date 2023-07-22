added new state of answers -> fixed some issues with a masking of repeated questions, prepared for the feature of planing questions

schedule.py
@@ -101,32 +101,6 @@ def random_question(person_id: int, group_id: list or int = None) -> int:
             return -1
 
 
-def repetition_function(person_id: int, now=datetime.datetime.now(), period_function=math.exp, repetition_amount=6):
-    db = create_session()
-    answers_map = defaultdict(list)
-    questions_to_ask = []
-    questions_to_ask_now = []
-
-    answers_timeline = db.scalars(
-        select(questions.QuestionAnswer).where(and_(questions.QuestionAnswer.person_id == person_id,
-                                                    questions.QuestionAnswer.state == questions.AnswerState.ANSWERED)).order_by(
-            questions.QuestionAnswer.ask_time.desc())).all()
-
-    for answer in answers_timeline:
-        answers_map[answer.question_id].append(answer)
-
-    for key in answers_map:
-        length = len(answers_map[key])
-        if length < repetition_amount:
-            delta = datetime.timedelta(period_function(length))
-            questions_to_ask.append((key, delta))
-            if delta + answers_map[key][0].ask_time - datetime.timedelta(seconds=1) <= now <= \
-                    delta + answers_map[key][0].ask_time + datetime.timedelta(seconds=1):
-                questions_to_ask_now.append(key)
-
-    return questions_to_ask_now, questions_to_ask
-
-
 class WeekDays(enum.Enum):
     Monday = 0
     Tuesday = 1
@@ -141,7 +115,7 @@ class Schedule(Thread):
     def __init__(self, callback):
         super().__init__()
         self._callback = callback
-        self._every = {}
+        self._every = None
         self._order = None  # 1 if time period is calculated first and 0 in other case
         self._week_days = None
         self._distribution_function = None
@@ -150,10 +124,8 @@ class Schedule(Thread):
         self._to_time = None
 
     def every(self, seconds: float = 0, minutes: float = 0, hours: float = 0,
-              days: float = 0, weeks: float = 0, months: float = 0):
-        if months != 0:
-            self._every['months'] = months
-        self._every['delta'] = datetime.timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days, weeks=weeks)
+              days: float = 0, weeks: float = 0):
+        self._every = datetime.timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days, weeks=weeks)
 
         if self._order is None:
             self._order = 1
@@ -177,7 +149,7 @@ class Schedule(Thread):
         return self
 
     def from_settings(self):
-        self._every = {'delta': Settings()['time_period']}
+        self._every = Settings()['time_period']
         self._order = Settings()['order']
         self._week_days = Settings()['week_days']
         self._distribution_function = Settings()['distribution_function']
@@ -198,8 +170,7 @@ class Schedule(Thread):
             now = datetime.datetime.now()
             if self._repetition_amount is not None:
                 for person in persons:
-                    questions_to_ask_now = \
-                        repetition_function(person.id, now, self._distribution_function, self._repetition_amount)[0]
+                    questions_to_ask_now = self.repetition_function(person.id, now)
                     if len(questions_to_ask_now) != 0:
                         question_for_person[person.id] = np.random.choice(questions_to_ask_now)
                     else:
@@ -211,34 +182,104 @@ class Schedule(Thread):
                 previous_call = self._periodic_call(now, previous_call, question_for_person)
             time.sleep(1)
 
+    def repetition_function(self, person_id: int, now=datetime.datetime.now()):
+        db = create_session()
+        answers_map = defaultdict(list)
+        questions_to_ask = []
+
+        answered_questions = db.scalars(
+            select(questions.QuestionAnswer).where(and_(questions.QuestionAnswer.person_id == person_id,
+                                                        questions.QuestionAnswer.state == questions.AnswerState.ANSWERED)).order_by(
+                questions.QuestionAnswer.ask_time.desc())).all()
+
+        planned_questions = db.scalars(
+            select(questions.QuestionAnswer).where(questions.QuestionAnswer.person_id == person_id).where(
+                questions.QuestionAnswer.state == questions.AnswerState.NOT_ANSWERED).order_by(
+                questions.QuestionAnswer.ask_time.desc())).all()
+
+        transferred_questions = db.scalars(
+            select(questions.QuestionAnswer).where(questions.QuestionAnswer.person_id == person_id).where(
+                questions.QuestionAnswer.state == questions.AnswerState.TRANSFERRED).order_by(
+                questions.QuestionAnswer.ask_time.desc())).all()
+
+        for answer in planned_questions:  # Add questions which were planned to ask before to the list
+            questions_to_ask.append(answer.question_id)
+
+        for answer in answered_questions:  # Add questions which were not planned yet to the map of questions for repeat
+            if answer.question_id not in questions_to_ask:
+                answers_map[answer.question_id].append(answer)
+
+        for answer in transferred_questions:  # Add questions which were transferred to the database as planed
+            if answer.question_id not in questions_to_ask:
+                answer.state = questions.AnswerState.INCORRECT
+
+                planned_answer = questions.QuestionAnswer()
+                planned_answer.ask_time = now + self._every
+                planned_answer.state = questions.AnswerState.NOT_ANSWERED
+                planned_answer.question_id = answer.question_id
+                planned_answer.person_id = person_id
+                db.add(planned_answer)
+
+        for key in answers_map:  # Add questions which require repetition to the database as planed
+            length = len(answers_map[key])
+            if length < self._repetition_amount:
+                delta = datetime.timedelta(self._distribution_function(length))
+                if delta + answers_map[key][0].ask_time <= now:
+                    planned_answer = questions.QuestionAnswer()
+                    planned_answer.ask_time = now
+                    planned_answer.state = questions.AnswerState.NOT_ANSWERED
+                    planned_answer.question_id = key
+                    planned_answer.person_id = person_id
+                    db.add(planned_answer)
+
+        db.commit()
+        return questions_to_ask
+
     def _periodic_call(self, now, previous_call, args):
         if self._from_time is None or self._from_time <= now.time() <= self._to_time:
             if self._order == 1:
-                if previous_call is None or ('month' in self._every.keys() and (
-                        (previous_call.month + self._every['month']) % 12 + 1) >= now.month):
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
+                if previous_call is None or (now >= previous_call + self._every):
+                    previous_call = now
+                    if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
+                        self._send_to_people(args, now)
                         previous_call = now
-                        if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                            self._send_to_people(args)
-                            previous_call = now
-                elif 'month' not in self._every.keys():
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
-                        previous_call = now
-                        if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                            self._send_to_people(args)
-                            previous_call = now
             if self._order == 0:
                 if self._week_days is None or (WeekDays(now.weekday()) in self._week_days):
-                    if previous_call is None or (now >= previous_call + self._every['delta']):
-                        self._send_to_people(args)
+                    if previous_call is None or (now >= previous_call + self._every):
+                        self._send_to_people(args, now)
                         previous_call = now
 
         return previous_call
 
-    def _send_to_people(self, question_to_person):
+    def _send_to_people(self, question_to_person, now):
         with create_session() as db:
             for person_id in question_to_person:
-                question_id = question_to_person[person_id]
+                question_id = int(question_to_person[person_id])
+                person_id: int
+
                 question = db.get(questions.Question, int(question_id))
                 person = db.get(users.Person, person_id)
-                self._callback(person, question)
+                person_answer = self._callback(person, question)
+                # person_answer = 1
+
+                planned_question = db.scalar(select(questions.QuestionAnswer).where(
+                    questions.QuestionAnswer.person_id == person_id).where(
+                    questions.QuestionAnswer.question_id == question_id).where(
+                    questions.QuestionAnswer.state == questions.AnswerState.NOT_ANSWERED))
+                if planned_question is not None:
+                    planned_question.person_answer = person_answer
+                    planned_question.state = questions.AnswerState.ANSWERED if person_answer == question.answer else \
+                        questions.AnswerState.TRANSFERRED
+                    planned_question.answer_time = datetime.datetime.now()
+                    db.commit()
+                else:
+                    planned_question = questions.QuestionAnswer()
+                    planned_question.answer_time = datetime.datetime.now()
+                    planned_question.ask_time = now
+                    planned_question.question_id = question_id
+                    planned_question.person_answer = person_answer
+                    planned_question.person_id = person_id
+                    planned_question.state = questions.AnswerState.ANSWERED if person_answer == question.answer else \
+                        questions.AnswerState.TRANSFERRED
+                    db.add(planned_question)
+            db.commit()