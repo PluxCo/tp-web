5606eff0585918adc7d7e1078637195f173cd06b
enard <serg.miass340@gmail.com>
2023-07-25 14:03:00 +0500

    added some settings forms

--- web/forms/settings.py
+++ web/forms/settings.py
@@ -1,15 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SelectField, IntegerField
+from wtforms.fields import StringField, SelectMultipleField, IntegerField, TimeField
 from wtforms.validators import DataRequired
+from schedule import WeekDays
 
 
-class SettingsForm(FlaskForm):
-    protection_type = SelectField("Protection type",
-                                  choices=[("none", "None"),
-                                           ("single", "Select digit"),
-                                           ("single_random", "Select random digit"),
-                                           ("text", "Type text"),
-                                           ("random_num", "Type random number")])
-    protection_key = StringField("Protection key")
+class TelegramSettingsForm(FlaskForm):
+    tg_pin = StringField("Telegram auth pin")
 
-    bot_timeout = IntegerField("Bot timeout (s)")
+
+class ScheduleSettingsForm(FlaskForm):
+    time_period = StringField("Period")
+    week_days = SelectMultipleField("Week days", choices=[(WeekDays.Monday, "Monday"),
+                                                          (WeekDays.Tuesday, "Tuesday"),
+                                                          (WeekDays.Wednesday, "Wednesday"),
+                                                          (WeekDays.Thursday, "Thursday"),
+                                                          (WeekDays.Friday, "Friday"),
+                                                          (WeekDays.Saturday, "Saturday"),
+                                                          (WeekDays.Sunday, "Sunday"), ])
+    from_time = TimeField("From")
+    to_time = TimeField("To")
