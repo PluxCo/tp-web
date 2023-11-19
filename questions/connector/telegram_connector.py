e59c81f1e8bcadb6c058f2e5269577f0c6131787
Anton Chuvashov <antonchuvashow@gmail.com>
2023-11-19 12:30:17 +0500

    added Message to TelegramConnector

--- questions/connector/telegram_connector.py
+++ questions/connector/telegram_connector.py
@@ -1,4 +1,5 @@
 import abc
+import json
 
 import flask_restful
 
@@ -19,7 +20,20 @@ class TelegramConnector(ConnectorInterface):
         self.alive_sessions: list[Session] = []
 
     def transfer(self, sessions: list[Session]):
-        print(sessions)
+        request = {"webhook": "",
+                   "messages": []}
+        for session in sessions:
+            current_question = session.next_question()
+            message = {"user_id": current_question.person_id,
+                       "type": "WITH_BUTTONS",
+                       "data": {
+                           "text": current_question.question.text,
+                           "buttons": json.loads(current_question.question.options)
+                       }
+                       }
+            request["messages"].append(message)
+
+        print(request)
 
 
 class TelegramWebhookResource(flask_restful.Resource):
