b8bd080290f748c28454b60cc9e0829d065d127e
enard <serg.miass340@gmail.com>
2023-12-27 21:32:51 +0500

    fixed open questions creation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from web import app, socketio
 
@@ -7,4 +8,4 @@ logging.basicConfig(level="DEBUG")
 app.config['SECRET_KEY'] = 'secret_key'
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", allow_unsafe_werkzeug=True, port=3002)
+    socketio.run(app, host="0.0.0.0", allow_unsafe_werkzeug=True, port=int(os.getenv("PORT", "3000")))
